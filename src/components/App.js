c22c84f41fe7c213358955988d524bbfa58493c2
dain-Yu <dakyo1990@naver.com>
2020-01-21 19:18:32 +0900

    navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import AppShell from './AppShell';
 
 class App extends React.Component {
-render() {
-return (
-<div>
-<h3>Hello World</h3>
-</div>
-);
-}
+    render() {
+        return (
+            //사용자가 처음 접속하는 페이지 
+            // <div>
+            //    <h3>Hello World</h3>
+            // </div>
+            <AppShell />
+        );
+    }
 }
 
 export default App;
